e594b48a29beb1f1b64f37821fd2cff09195daea
enji1115 <jonji1115@gmail.com>
2026-09-15 10:53:08 +0900

    JavaScript 학습 > 변수

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-console.log('Hello JS');
+console.log("Hello JS");
 
 // 변수: 값을 저장하는 이름을 가진 공간
-const username = '조은지';
-console.log('username', username);
+// const username = "조은지";
+let username = "조은지";
+username = "홍길동";
+console.log("username", username);
 
-// vscode.dev 웹 VS Code 테스트
+// 데이터타입
+const num = 10; // 숫자
+console.log("🚀 ~ num:", num, "num type", typeof num);
+
+const text = "안녕하세요"; // 문자
+console.log("🚀 ~ text:", text, "text type", typeof text);
+
+const isDarkMode = false; // boolean
+// const isDarkMode = "false"; // 문자열
+console.log("🚀 ~ isDarkMode:", isDarkMode)
+console.log("🚀 ~ text:", isDarkMode, "isDarkMode type", typeof isDarkMode);
+
+const empty = null; // 초기화 할 때 사용
+console.log("🚀 ~ empty:", empty, "empty type", typeof empty); // object - 기본적으로 변수는 객체로 되어있음
+
+let userage; // 번수 선언만
+console.log("🚀 ~ userage:", userage, "userage type", typeof userage)
